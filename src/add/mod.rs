4d0db829480e9032e3dc2c97a2b40f9e3b6846a6
John Blackburn <jackblackburnn@icloud.com>
2025-01-20 15:46:19 -0500

    Add output path as hardcoded constant

--- src/add/mod.rs
+++ src/add/mod.rs
@@ -8,14 +8,19 @@ use image::{ImageBuffer, Rgb};
 
 type RgbImageBuffer = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
+
+
+const OUTPUT_PATH: &str = "./outputs/out-brg.png"; 
+
 pub fn start(image_buffer: RgbImageBuffer) {
 
         // clone image buffer contents
     let mut image_clone = image_buffer.clone();
     println!("Continuing in add mode"); 
 
-        // add changes to mutable clone
+        // add changes to mutable clone and save to output path
     swap::apply("brg", &mut image_clone);
-    image_clone.save("./outputs/out.png");
+
+    let _ = image_clone.save(OUTPUT_PATH);
 
 }   
