d45040cd731d2b1e9a014587910c2b749f87dd1d
John Blackburn <jackblackburnn@icloud.com>
2025-01-22 16:45:53 -0500

    Apply invert to image

--- src/add/mod.rs
+++ src/add/mod.rs
@@ -3,14 +3,14 @@
 //
 
 use crate::filters::swap;
+use crate::filters::invert;
 
 use image::{ImageBuffer, Rgb};
 
 type RgbImageBuffer = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
 
-
-const OUTPUT_PATH: &str = "./outputs/out-brg.png"; 
+const OUTPUT_PATH: &str = "./outputs/matts-creek-fire-invert-rbb.png"; 
 
 pub fn start(image_buffer: RgbImageBuffer) {
 
@@ -19,7 +19,9 @@ pub fn start(image_buffer: RgbImageBuffer) {
     println!("Continuing in add mode"); 
 
         // add changes to mutable clone and save to output path
-    swap::apply("brg", &mut image_clone);
+    swap::apply("rbb", &mut image_clone);
+    invert::apply(&mut image_clone);
+
 
     let _ = image_clone.save(OUTPUT_PATH);
 
