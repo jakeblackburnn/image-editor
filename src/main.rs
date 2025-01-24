18200dc533dfc24e3239281e3bc27471afadac68
John Blackburn <jackblackburnn@icloud.com>
2025-01-23 20:12:39 -0500

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,13 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
+        // Required Arguments
+        // mode and image path are always 1st and 2nd arguments
     let mode       = args.get(1).map(String::as_str).expect("no mode specified");
     let image_path = args.get(2).map(String::as_str).expect("no image path specified");
 
-
+        // Optional Arguments
+        // nothing here yet...
 
     println!("Attempting to load image buffer: {}", image_path);
     let image_buffer: RgbImageBuffer = image::open(image_path)
