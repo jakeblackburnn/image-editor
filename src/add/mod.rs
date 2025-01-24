181af02d654b5eae8d6167578fe3c4702997dd40
John Blackburn <jackblackburnn@icloud.com>
2025-01-23 19:54:53 -0500

    Remove hardcoded filtering

--- src/add/mod.rs
+++ src/add/mod.rs
@@ -2,27 +2,14 @@
 // Created by J. Blackburn - Jan 19 2025
 //
 
-use crate::filters::swap;
-use crate::filters::invert;
-
 use image::{ImageBuffer, Rgb};
 
 type RgbImageBuffer = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
 
-const OUTPUT_PATH: &str = "./outputs/matts-creek-fire-invert-rbb.png"; 
 
 pub fn start(image_buffer: RgbImageBuffer) {
 
-        // clone image buffer contents
-    let mut image_clone = image_buffer.clone();
     println!("Continuing in add mode"); 
-
-        // add changes to mutable clone and save to output path
-    swap::apply("rbb", &mut image_clone);
-    invert::apply(&mut image_clone);
-
-
-    let _ = image_clone.save(OUTPUT_PATH);
 
 }   
