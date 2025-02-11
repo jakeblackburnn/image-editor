4f6a217ff4b9a241ee664a3149590a7cf5ab1525
John Blackburn <jackblackburnn@icloud.com>
2025-02-10 19:23:34 -0500

    Add basic filter trait

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -4,3 +4,15 @@
 
 pub mod swap;
 pub mod invert;
+
+use image::{ImageBuffer, Rgb};
+
+type RgbImageBuffer = ImageBuffer<Rgb<u8>, Vec<u8>>;
+
+
+
+pub trait Filter {
+
+    fn apply(&self, image: RgbImageBuffer);
+
+}
