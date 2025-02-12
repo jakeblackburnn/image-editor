739ebd44df290fff1f216a6410f5d435a378bd38
John Blackburn <jackblackburnn@icloud.com>
2025-02-11 19:50:21 -0500

    Add filter factory skeleton

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -6,13 +6,32 @@ pub mod swap;
 pub mod invert;
 
 use image::{ImageBuffer, Rgb};
+use std::collections::HashMap;
 
 type RgbImageBuffer = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
 
 
 pub trait Filter {
-
     fn apply(&self, image: RgbImageBuffer);
+}
 
+
+struct FilterFactory {
+
+    registry: HashMap<String, fn() -> Box<dyn Filter>>, // maps filter name to function that returns
+                                                       // corresponding filter struct
+}
+
+
+impl FilterFactory {
+    fn new() -> Self {
+        let registry = HashMap::new();
+
+        // Add filter creators to registry
+     
+        Self {
+            registry,
+        }
+    }
 }
