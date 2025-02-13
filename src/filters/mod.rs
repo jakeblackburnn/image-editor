8a83e6034147582c4fd59329b02674e19ef70937
John Blackburn <jackblackburnn@icloud.com>
2025-02-13 18:39:04 -0500

    Change filter factory hashmap name

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -13,25 +13,25 @@ type RgbImageBuffer = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
 
 pub trait Filter {
-    fn apply(&self, image: RgbImageBuffer);
+    fn apply(&self, image: &mut RgbImageBuffer);
 }
 
 
 struct FilterFactory {
 
-    registry: HashMap<String, fn() -> Box<dyn Filter>>, // maps filter name to function that returns
-                                                       // corresponding filter struct
+    filter_constructors: HashMap<String, fn() -> Box<dyn Filter>>, // maps filter name to function that 
+                                                        // returns corresponding filter struct
 }
 
 
 impl FilterFactory {
     fn new() -> Self {
-        let registry = HashMap::new();
+        let filter_constructors = HashMap::new();
 
         // Add filter creators to registry
      
         Self {
-            registry,
+            filter_constructors,
         }
     }
 }
