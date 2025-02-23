d9f1cee563fdddda32c78b4888d3127f0ef4af70
John Blackburn <jackblackburnn@icloud.com>
2025-02-22 23:45:20 -0500

    Add get filter method to factory struct

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -17,7 +17,7 @@ pub trait Filter {
 }
 
 
-struct FilterFactory {
+pub struct FilterFactory {
 
     filter_constructors: HashMap<String, fn() -> Box<dyn Filter>>, // maps filter name to function that 
                                                                    // returns corresponding filter struct
@@ -25,7 +25,7 @@ struct FilterFactory {
 
 
 impl FilterFactory {
-    fn new() -> Self {
+    pub fn new() -> Self {
         let mut filter_constructors: HashMap<String, fn() -> Box<dyn Filter>> = HashMap::new();
 
         filter_constructors.insert("invert".to_string(), invert::construct);
@@ -33,5 +33,9 @@ impl FilterFactory {
         Self {
             filter_constructors,
         }
+    }
+
+    pub fn get(&self, filter: &str) -> fn() -> Box<dyn Filter> {
+        self.filter_constructors[filter]
     }
 }
