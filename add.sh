f2e5047340650a37cb958aba7a11b203ae8d44ba
John Blackburn <jackblackburnn@icloud.com>
2025-02-23 11:48:37 -0500

    Add basic add script & in/out folders

---
+++ add.sh
@@ -0,0 +1,8 @@
+#
+# Created by J. Blackburn - Feb 23 2025
+
+
+# For this to work, directories for in/ and out/ are required to exist
+
+	# simple example run
+cargo run -- add in/iris.jpg invert out/out.png
