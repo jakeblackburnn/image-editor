3ba7981ef951343a3f79be691004c026cafb5892
John Blackburn <jackblackburnn@icloud.com>
2025-02-23 11:36:26 -0500

    Switch to user specified filter and save path

--- src/add/mod.rs
+++ src/add/mod.rs
@@ -9,17 +9,28 @@ type RgbImageBuffer = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
 
 
-pub fn start(mut image_buffer: RgbImageBuffer, filter: Option<&String>, save_path: Option<&String>) {
+pub fn start(mut image_buffer: RgbImageBuffer, filter_arg: Option<&String>, save_path_arg: Option<&String>) {
 
+        // For now these arguments are required
+    let filter_name    =     filter_arg.map(String::as_str).expect("no filter specified");
+    let save_path      =  save_path_arg.map(String::as_str).expect("no save path specified");
+
+
+        // Get specified filter from factory
     let factory        = filters::FilterFactory::new();
-    let filter_builder = factory.get("invert");
+    let filter_builder = factory.get(filter_name);
+    let filter = filter_builder();
 
-    filter_builder().apply(&mut image_buffer);
+        // Apply dynamically loaded filter to image buffer
+    filter.apply(&mut image_buffer);
 
+
+
+        // Save the modified image
 
         println!("saving image. . .");
 
-    let _  = image_buffer.save("outputs/out.png");
+    let _  = image_buffer.save(save_path);
 
         println!("image saved.");
 }   
