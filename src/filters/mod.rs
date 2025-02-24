e6eb6023833662e08619328e33db1110ab7d613f
John Blackburn <jackblackburnn@icloud.com>
2025-02-23 20:45:54 -0500

    Add key_string argument to filter constructors

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -19,23 +19,25 @@ pub trait Filter {
 
 pub struct FilterFactory {
 
-    filter_constructors: HashMap<String, fn() -> Box<dyn Filter>>, // maps filter name to function that 
-                                                                   // returns corresponding filter struct
+    filter_constructors: HashMap<String, fn(key_string: &str) -> Box<dyn Filter>>, // maps filter name to function that 
+                                                                                   // returns corresponding filter struct
+                                                                                   // functions must accept a keystring, even if its empty
 }
 
 
 impl FilterFactory {
     pub fn new() -> Self {
-        let mut filter_constructors: HashMap<String, fn() -> Box<dyn Filter>> = HashMap::new();
+        let mut filter_constructors: HashMap<String, fn(key_string: &str) -> Box<dyn Filter>> = HashMap::new();
 
         filter_constructors.insert("invert".to_string(), invert::construct);
+        filter_constructors.insert("swap".to_string(), swap::construct);
      
         Self {
             filter_constructors,
         }
     }
 
-    pub fn get(&self, filter: &str) -> fn() -> Box<dyn Filter> {
+    pub fn get(&self, filter: &str) -> fn(key_string: &str) -> Box<dyn Filter> {
         self.filter_constructors[filter]
     }
 }
