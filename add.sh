e57c3eb1a93ce52b79642a8451e8e9ba663c7a98
John Blackburn <jackblackburnn@icloud.com>
2025-02-23 22:01:55 -0500

    Update example add script

--- add.sh
+++ add.sh
@@ -1,8 +1,10 @@
 #
 # Created by J. Blackburn - Feb 23 2025
+#
 
 
 # For this to work, directories for in/ and out/ are required to exist
 
 	# simple example run
-cargo run -- add in/iris.jpg invert out/out.png
+cargo run -- add in/iris.jpg swap-brg out/out.png
+cargo run -- add out/out.png invert- out/out.png
