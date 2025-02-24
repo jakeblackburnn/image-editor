6ac4924d2e549a03d2b0d7aba23bc6169fb33865
John Blackburn <jackblackburnn@icloud.com>
2025-02-23 22:08:35 -0500

    Rename example output paths

--- add.sh
+++ add.sh
@@ -6,5 +6,5 @@
 # For this to work, directories for in/ and out/ are required to exist
 
 	# simple example run
-cargo run -- add in/iris.jpg swap-brg out/out.png
-cargo run -- add out/out.png invert- out/out.png
+cargo run -- add in/iris.jpg swap-brg out/out1.png
+cargo run -- add out/out1.png invert- out/out2.png
