b9b2cdd2f1c3090d66456ad8c3041b2911d3880e
John Blackburn <jackblackburnn@icloud.com>
2025-03-06 17:40:57 -0500

    Add batch output run

--- src/add/runs.rs
+++ src/add/runs.rs
@@ -7,12 +7,12 @@ use crate::utils::load_rgb_image_buffer;
 use crate::filters;
 
 
-pub fn single_run(input_image: String, filter: String, output_path: String) {
+pub fn single_run(input_image: String, filter_string: String, output_path: String) {
 
     let mut image_buffer = load_rgb_image_buffer(input_image);
 
-        // break filter identifier into name and key
-    let (filter_name, key_string) = filters::get_filter_components(&filter);
+        // break filter string into name and key
+    let (filter_name, key_string) = filters::get_filter_components(&filter_string);
 
         // build filter via filter factory
     let factory = filters::FilterFactory::new();
@@ -25,4 +25,36 @@ pub fn single_run(input_image: String, filter: String, output_path: String) {
 
     let _ = image_buffer.save(output_path);
 
+}
+
+
+pub fn batch_output_run(input_image: String, filter_set_string: String, output_dir: String) {
+    
+    let image_buffer = load_rgb_image_buffer(input_image);
+
+    let filter_set = filters::keysets::get_filter_set(filter_set_string);
+
+    let factory = filters::FilterFactory::new();
+
+    for filter_string in filter_set {
+
+            // create mutable clone of base image
+        let mut image_buffer_clone = image_buffer.clone();
+
+            // break filter string into name and key
+        let (filter_name, key_string) = filters::get_filter_components(&filter_string);
+
+            // build filter via filter factory
+        let filter_builder = factory.get(filter_name);
+        let filter = filter_builder(key_string);
+
+
+
+        filter.apply(&mut image_buffer_clone);
+
+            // create output path string, default is output_dir/filter.png
+        let output_path = format!("{}/{}.png", output_dir, filter_string);
+
+        let _ = image_buffer_clone.save(output_path);
+    }
 }
