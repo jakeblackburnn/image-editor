b00d834b87ab9915191d26c65b8a692a89b374a4
John Blackburn <jackblackburnn@icloud.com>
2025-03-06 17:00:47 -0500

    Add get filter set from string

--- src/filters/keysets/mod.rs
+++ src/filters/keysets/mod.rs
@@ -2,6 +2,15 @@
 // Created By J. Blackburn - Mar 6 2025
 //
 
+pub fn get_filter_set(filter_set: String) -> Vec<String> {
+
+    match filter_set.as_str() {
+        "basic-swaps" => get_basic_swaps(),
+
+        _ => panic!("couldnt get filter set"),
+    }
+}
+
     // keys for keyset identifier: 'basic-swaps'
 pub fn get_basic_swaps() -> Vec<String> {
 
