cb6df5745f90765fd2a34b34ee7f4e95bc375632
John Blackburn <jackblackburnn@icloud.com>
2025-03-29 21:02:05 -0400

    Add load image buffer to view mode

--- src/view/mod.rs
+++ src/view/mod.rs
@@ -2,13 +2,27 @@
 // Created By J. Blackburn - Jan 20 2025
 //
 
+use crate::utils::load_rgb_image_buffer;
+
+use eframe::egui;
 use image::{ImageBuffer, Rgb};
+
+use std::sync::{Arc, Mutex};
 
 type RgbImageBuffer = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
+type SharedImageBuffer = Arc<Mutex<RgbImageBuffer>>;
 
 
-pub fn start(image_buffer: RgbImageBuffer, filter_name: Option<&String>, save_path: Option<&String>) {
+
+pub fn start(mut args: std::env::Args) {
+
+        // load base image buffer
+    let input_image = args.next().expect("Failed to parse image path");
+    let image_buffer = load_rgb_image_buffer(input_image);
+
+        // create shareable clone of image
+    let mut sharedImageBuffer = image_buffer.clone();
 
     println!("continuing in view mode");
 
