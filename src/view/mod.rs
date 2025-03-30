fa33d33a4262049842098fc244b097c6e8d8fd20
John Blackburn <jackblackburnn@icloud.com>
2025-03-29 21:50:44 -0400

    Add practical test of basic unresponsive view panel

--- src/view/mod.rs
+++ src/view/mod.rs
@@ -2,16 +2,21 @@
 // Created By J. Blackburn - Jan 20 2025
 //
 
+mod view_panel;
+use view_panel::ViewPanel;
+
 use crate::utils::load_rgb_image_buffer;
+
 
 use eframe::egui;
 use image::{ImageBuffer, Rgb};
 
 use std::sync::{Arc, Mutex};
 
-type RgbImageBuffer = ImageBuffer<Rgb<u8>, Vec<u8>>;
+type RgbImageBuffer    = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
 type SharedImageBuffer = Arc<Mutex<RgbImageBuffer>>;
+type SharedBool        = Arc<Mutex<bool>>;
 
 
 
@@ -21,9 +26,16 @@ pub fn start(mut args: std::env::Args) {
     let input_image = args.next().expect("Failed to parse image path");
     let image_buffer = load_rgb_image_buffer(input_image);
 
-        // create shareable clone of image
-    let mut sharedImageBuffer = image_buffer.clone();
+        // create shareable clone of image, and update switch
+    let mut shared_image_buffer = Arc::new( Mutex::new( image_buffer.clone() ));
+    let mut update_switch = Arc::new( Mutex::new( true ));
 
-    println!("continuing in view mode");
+    let mut app = ViewPanel::new( shared_image_buffer.clone(), update_switch.clone() );
+
+    let _ = eframe::run_native(
+        "Image Editor View Panel",
+        eframe::NativeOptions::default(),
+        Box::new( |_cc| Ok(Box::new(app)))
+    );
 
 }
