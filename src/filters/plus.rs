8398dcb494efe25d571558970f9f928bb72b0c32
John Blackburn <jackblackburnn@icloud.com>
2025-04-08 19:22:03 -0400

    Add single color plus

--- src/filters/plus.rs
+++ src/filters/plus.rs
@@ -6,18 +6,34 @@
 use super::Filter; // pull down filter trait from mod.rs
 
 use image::{ImageBuffer, Rgb};
+use regex::Regex;
 
 type RgbImageBuffer = ImageBuffer<Rgb<u8>, Vec<u8>>;
 
 
 pub struct Plus {
     n: u8,
+    color: Option<char>,
 }
 
 pub fn construct(key_string: &str) -> Box<dyn Filter> {
-    let n: u8 = key_string.parse().expect("Failed to parse key string to u32");
 
-    Box::new( Plus { n } )
+    let re = Regex::new(r"(?P<color>[rgb])?(?P<n>[0-9]+)")
+                    .unwrap();
+
+    let captures = re.captures(key_string).expect("Failed to parse plus key string");
+
+    let n_str = captures.name("n").unwrap().as_str();
+
+    let color = captures.name("color").map(|c| c.as_str()
+                                                .chars()
+                                                .next()
+                                                .unwrap() );
+
+
+
+    let n: u8 = n_str.parse().expect("Failed to parse key string to u8");
+    Box::new( Plus { n, color } )
 }
 
 impl Filter for Plus {
@@ -30,7 +46,16 @@ impl Filter for Plus {
         for x in 0..width  {
 
             let pixel     = image_buffer.get_pixel(x, y);
-            let new_pixel = add_to_pixel(pixel, self.n);
+
+            let mut new_pixel = Rgb([0,0,0]);
+
+            match self.color { 
+                None      => { new_pixel = add_to_pixel(pixel, self.n); },
+                Some('r') => { new_pixel = add_to_r(pixel, self.n); },
+                Some('g') => { new_pixel = add_to_g(pixel, self.n); },
+                Some('b') => { new_pixel = add_to_b(pixel, self.n); },
+                _ => { panic!("plus failed due to unexpected color value!?!?"); },
+            }
 
             image_buffer.put_pixel(x, y, new_pixel);
 
@@ -49,3 +74,28 @@ fn add_to_pixel(pixel: &Rgb<u8>, n: u8) -> Rgb<u8> {
 
     Rgb([ newr, newg, newb ])
 }
+
+fn add_to_r(pixel: &Rgb<u8>, n: u8) -> Rgb<u8> {
+    let (r, g, b) = (pixel[0], pixel[1], pixel[2]);
+
+    let newr = r.saturating_add(n);
+
+    Rgb([ newr, g, b ])
+}
+
+fn add_to_g(pixel: &Rgb<u8>, n: u8) -> Rgb<u8> {
+    let (r, g, b) = (pixel[0], pixel[1], pixel[2]);
+
+    let newg = g.saturating_add(n);
+
+    Rgb([ r, newg, b ])
+}
+
+fn add_to_b(pixel: &Rgb<u8>, n: u8) -> Rgb<u8> {
+    let (r, g, b) = (pixel[0], pixel[1], pixel[2]);
+
+    let newb = b.saturating_add(n);
+
+    Rgb([ r, g, newb ])
+}
+
