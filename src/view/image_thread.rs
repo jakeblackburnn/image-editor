b8355b3dd28ed5fa3f5934bab27b9430e4d9a11a
John Blackburn <jackblackburnn@icloud.com>
2025-05-29 19:12:01 -0400

    split menu options into separate functions

--- src/view/image_thread.rs
+++ src/view/image_thread.rs
@@ -11,19 +11,9 @@ use std::thread;
 use std::path::Path;
 use std::io::{stdin, stdout, Write};
 
-
-
 type RgbImageBuffer    = ImageBuffer<Rgb<u8>, Vec<u8>>;
 type SharedImageBuffer = Arc<Mutex<RgbImageBuffer>>;
 type SharedBool        = Arc<Mutex<bool>>;
-
-#[derive(PartialEq)]
-enum MenuOption { // types of possible user inputs
-    Empty, 
-    Filter, 
-    Save, 
-    Quit,
-}
 
 pub fn start_image_thread( image_buffer: SharedImageBuffer, update_switch: SharedBool ) {
 
@@ -31,82 +21,88 @@ pub fn start_image_thread( image_buffer: SharedImageBuffer, update_switch: Share
 
         // TODO: Format txt output
 
-        let mut menu_option = MenuOption::Empty; // default option is none / empty
-        
-        let factory = filters::FilterFactory::new();
 
+        loop {
+            print!("enter option: ");
+            let _ = stdout().flush(); 
 
-        while menu_option != MenuOption::Quit {
+            let input = read_input();
 
-                // TODO: Add proper input menu
-            println!("enter option: ");
-
-            let mut input = String::new();
-            stdin().read_line(&mut input).expect("Failed to read user input");
-
-            let _ = stdout().flush(); // toss junk 
-
-            menu_option = match input.trim() {
-                "f" => MenuOption::Filter,
-                "s" => MenuOption::Save,
-                "q" => MenuOption::Quit,
-                 _  => MenuOption::Empty,
-            };
-
-                // TODO: implement menu option actions
-            match menu_option {
-
-                MenuOption::Filter => {
-                    print!("Filtering image - Enter filter identifier: ");
-                    let _ = stdout().flush();
-
-                    let mut filter_string = String::new();
-                    stdin().read_line(&mut filter_string)
-                           .expect("Error: could not read filter identifier");
-                    let _ = stdout().flush();
-
-                        // break filter string into name and key
-                    let (filter_name, key_string) = 
-                        filters::get_filter_components(&filter_string);
-
-
-                    let filter_builder = factory.get(filter_name);
-                    let filter = filter_builder(key_string);
-
-                    let mut locked_image_buffer = image_buffer.lock().unwrap();
-
-                    filter.apply(&mut locked_image_buffer);
-
-                    let mut locked_update_switch = update_switch.lock().unwrap();
-                    *locked_update_switch = true;
-
+            match input.as_str() {
+                "f" => {
+                    handle_filter_option( image_buffer.clone(), update_switch.clone() );
                     continue;
-                }
-                MenuOption::Save => {
-                    print!("Saving image - Specify output path: ");
-                    let _ = stdout().flush();
-
-                    let mut output_path = String::new();
-                    stdin().read_line(&mut output_path)
-                           .expect("Error: could not read user input");
-
-                    let _ = stdout().flush();
-
-                    let locked_image_buffer = image_buffer.lock().unwrap();
-                    locked_image_buffer.save(Path::new(output_path.trim())); 
-
+                },
+                "s" => {
+                    handle_save_option( image_buffer.clone() );
                     continue;
-                }
-                MenuOption::Quit => {
-                    println!("Quitting");
-                    continue;
-                }
-                MenuOption::Empty => {
+                },
+                 _  => {
                     println!("Option provided is invalid - try again");
                     continue;
-                }
-
-            } // end match menu option
-        } // end while not quit option
+                },
+                "q" => {
+                    println!("Quitting");
+                    break;
+                },
+            };
+        }
+                 
     }); // end spawn thread
+}
+
+fn read_input() -> String {
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Failed to read user input");
+    let _ = stdout().flush(); 
+    input.trim().to_string()
+}
+
+fn handle_filter_option(image_buffer: SharedImageBuffer, update_switch: SharedBool) {
+    print!("Filtering image - Enter filter identifier: ");
+    let _ = stdout().flush();
+
+    loop {
+        let filter_string = read_input();
+
+            // break filter string into name and key
+        match filters::get_filter_components(&filter_string) {
+            None => {
+                print!("failed to get filter components, try again: ");
+                let _ = stdout().flush();
+                continue;
+            },
+            Some((filter_name, key_string)) => {
+
+                let factory = filters::FilterFactory::new();
+                match factory.get(filter_name) {
+                    Some(filter_builder) => {
+                        
+                        let filter = filter_builder(key_string);
+                        let mut locked_image_buffer = image_buffer.lock().unwrap();
+                        filter.apply(&mut locked_image_buffer);
+
+                        let mut locked_update_switch = update_switch.lock().unwrap();
+                        *locked_update_switch = true;
+                        return;
+                    },
+                    None => {
+                        print!("failed to build filter, try again: ");
+                        let _ = stdout().flush();
+                        continue;
+                    },
+                }; // end match get filter
+            }, 
+        }; 
+    }
+}
+
+fn handle_save_option(image_buffer: SharedImageBuffer) {
+    print!("Saving image - Specify output path: ");
+    let _ = stdout().flush();
+
+    let output_path = read_input();
+
+    let locked_image_buffer = image_buffer.lock().unwrap();
+    locked_image_buffer.save(Path::new(&output_path)); 
 }
