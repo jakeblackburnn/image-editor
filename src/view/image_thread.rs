bdea9f6d61830f4ddb00338aa64815286391e54a
John Blackburn <jackblackburnn@icloud.com>
2025-06-04 14:24:02 -0400

    Refactor for new filter interface w/ error handling

--- src/view/image_thread.rs
+++ src/view/image_thread.rs
@@ -2,7 +2,7 @@
 // Created by J. Blackburn - Mar 30 2025
 //
 
-use crate::filters;
+use crate::filters::FilterFactory;
 
 use image::{ImageBuffer, Rgb};
 
@@ -15,19 +15,17 @@ type RgbImageBuffer    = ImageBuffer<Rgb<u8>, Vec<u8>>;
 type SharedImageBuffer = Arc<Mutex<RgbImageBuffer>>;
 type SharedBool        = Arc<Mutex<bool>>;
 
-pub fn start_image_thread( image_buffer: SharedImageBuffer, update_switch: SharedBool ) {
 
+
+pub fn start_image_thread( image_buffer: SharedImageBuffer, update_switch: SharedBool ) {
     thread::spawn( move || {
 
         // TODO: Format txt output
-
-
         loop {
             print!("enter option: ");
             let _ = stdout().flush(); 
 
             let input = read_input();
-
             match input.as_str() {
                 "f" => {
                     handle_filter_option( image_buffer.clone(), update_switch.clone() );
@@ -37,65 +35,58 @@ pub fn start_image_thread( image_buffer: SharedImageBuffer, update_switch: Share
                     handle_save_option( image_buffer.clone() );
                     continue;
                 },
+                "q" => {
+                    break;
+                },
                  _  => {
                     println!("Option provided is invalid - try again");
                     continue;
                 },
-                "q" => {
-                    println!("Quitting");
-                    break;
-                },
             };
         }
-                 
     }); // end spawn thread
 }
+
+
 
 fn read_input() -> String {
     let mut input = String::new();
     stdin().read_line(&mut input).expect("Failed to read user input");
     let _ = stdout().flush(); 
+
     input.trim().to_string()
 }
 
+
+
 fn handle_filter_option(image_buffer: SharedImageBuffer, update_switch: SharedBool) {
     print!("Filtering image - Enter filter identifier: ");
-    let _ = stdout().flush();
 
     loop {
+        let _ = stdout().flush();
+
         let filter_string = read_input();
+        let factory = FilterFactory::new();
 
-            // break filter string into name and key
-        match filters::get_filter_components(&filter_string) {
-            None => {
-                print!("failed to get filter components, try again: ");
-                let _ = stdout().flush();
+        match factory.get(&filter_string) {
+            Ok(filter) => {
+                let mut locked_image_buffer = image_buffer.lock().unwrap();
+                filter.apply(&mut locked_image_buffer);
+                let mut locked_update_switch = update_switch.lock().unwrap();
+                *locked_update_switch = true;
+
+                break;
+            }
+            Err(e) => { 
+                println!("Error: {}", e);
+                print!("try again: ");
                 continue;
-            },
-            Some((filter_name, key_string)) => {
-
-                let factory = filters::FilterFactory::new();
-                match factory.get(filter_name) {
-                    Some(filter_builder) => {
-                        
-                        let filter = filter_builder(key_string);
-                        let mut locked_image_buffer = image_buffer.lock().unwrap();
-                        filter.apply(&mut locked_image_buffer);
-
-                        let mut locked_update_switch = update_switch.lock().unwrap();
-                        *locked_update_switch = true;
-                        return;
-                    },
-                    None => {
-                        print!("failed to build filter, try again: ");
-                        let _ = stdout().flush();
-                        continue;
-                    },
-                }; // end match get filter
-            }, 
-        }; 
-    }
+            }
+        };
+    }; // end loop
 }
+
+
 
 fn handle_save_option(image_buffer: SharedImageBuffer) {
     print!("Saving image - Specify output path: ");
